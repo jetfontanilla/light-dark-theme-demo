19dd8395b7cff21d74049dd30a1f87cd72512406
Jet Fontanilla <jetlogs@gmail.com>
2021-08-19 10:10:52 +0900

    include JS approach as well for exmplanation

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,9 +4,9 @@ import style from './style.css';
 
 const Header = () => (
 	<header class={style.header}>
-		<h1>CSS prefers-color-scheme demo</h1>
+		<h1>Light / Dark theme demo</h1>
 		<nav>
-			<Link activeClassName={style.active} href="/">Basic</Link>
+			<Link activeClassName={style.active} href="/">Basics</Link>
 			<Link activeClassName={style.active} href="/demo">Demo</Link>
 			<Link activeClassName={style.active} href="/best-practice">Best Practices</Link>
 		</nav>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,27 +3,46 @@ import style from './style.css';
 
 const Home = () => (
 	<div class={style.home}>
-		<h1>prefers-color-scheme</h1>
+		<section>
+			<h1>CSS approach</h1>
 
-		<p>This media feature for CSS is used to detect if the user prefers a light or dark color theme based on their operating system setting (e.g. light or dark mode) or a user agent setting.</p>
+			<p><strong>prefers-color-scheme:</strong> This media feature for CSS is used to detect if the user prefers a light or dark color theme based on their operating system setting (e.g. light or dark mode) or a user agent setting.</p>
 
-		<h2>Syntax:</h2>
-		<div class={style.example}>
-			<pre>
-				@media (prefers-color-scheme: dark) {'{\n	...\n'}
-				{'}\n\n'}
+			<strong>Example:</strong>
+			<div className={style.example}>
+				<pre>
+					@media (prefers-color-scheme: dark) {'{\n'}
+					{'\t'}color: #fff;{'\n'}
+					{'\t'}background: #333;{'\n'}
+					{'}\n\n'}
 
-				@media (prefers-color-scheme: light) {'{\n	...\n'}
-				{'}'}
-			</pre>
-		</div>
+					@media (prefers-color-scheme: light) {'{\n'}
+					{'\t'}color: #000;{'\n'}
+					{'\t'}background: #fff;{'\n'}
+					{'}'}
+				</pre>
+			</div>
 
-		<p>
-			<strong>light</strong> - indicates that user has notified that they prefer an interface that has a light theme, or has not expressed an active preference.
-		</p>
-		<p>
-			<strong>dark</strong> - indicates that user has notified that they prefer an interface that has a dark theme.
-		</p>
+			<p>
+				<strong>light</strong> - indicates that user has notified that they prefer an interface that has a light theme, or has not expressed an active preference.<br/>
+				<strong>dark</strong> - indicates that user has notified that they prefer an interface that has a dark theme.
+			</p>
+		</section>
+
+		<section>
+			<h1>JavaScript approach</h1>
+
+			<p>use <strong>Window.matchMedia()</strong> to query the current document's media query list and match dark or light mode</p>
+
+			<strong>Example:</strong>
+			<div className={style.example}>
+				<pre>
+					const isLightTheme = window.matchMedia("(prefers-color-scheme: light)").matches{'\n'}
+					const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+				</pre>
+			</div>
+		</section>
+
 
 	</div>
 );
