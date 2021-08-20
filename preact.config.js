6b7dc37f2b70adbf756dabeaa43bacac01eb9454
Jet Fontanilla <jetlogs@gmail.com>
2021-08-20 10:16:23 +0900

    use custom webpack config for build output

---
+++ preact.config.js
@@ -0,0 +1,8 @@
+export default (config, env, helpers) => {
+    config.output.publicPath = "/light-dark-theme-demo/";
+    config.plugins.push(
+        new helpers.webpack.DefinePlugin({
+            PUBLIC_PATH: JSON.stringify(config.output.publicPath)
+        })
+    );
+};
