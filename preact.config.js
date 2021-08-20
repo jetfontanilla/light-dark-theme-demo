9002c28bc6c79c519517c30d0648731c27d44475
Jet Fontanilla <jetlogs@gmail.com>
2021-08-20 13:28:12 +0900

    use NODE_ENV for production vs dev builds

--- preact.config.js
+++ preact.config.js
@@ -1,8 +1,9 @@
 export default (config, env, helpers) => {
-    config.output.publicPath = "/light-dark-theme-demo/";
+    config.output.publicPath = process.env.NODE_ENV == "production" ? "/light-dark-theme-demo/" : "/";
     config.plugins.push(
         new helpers.webpack.DefinePlugin({
-            PUBLIC_PATH: JSON.stringify(config.output.publicPath)
+            PUBLIC_PATH: JSON.stringify(config.output.publicPath),
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
         })
     );
 };
